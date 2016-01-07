a2f58ebfe108db31f33d113ecfd756b2465b809a
Phil Dier <phil@raventools.com>
2016-01-07 13:36:18 -0600

    update version here too

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'phil@raventools.com'
 license          'MIT'
 description      'Manage php installation and configuration'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
