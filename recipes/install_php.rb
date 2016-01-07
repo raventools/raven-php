616a0b3ecf689f1859e78b87749c4f7ca4d73f66
Phil Dier <phil@raventools.com>
2016-01-07 13:12:58 -0600

    tweak php install

--- recipes/install_php.rb
+++ recipes/install_php.rb
@@ -2,27 +2,26 @@
 case node[:platform_family]
 when "rhel"
 
-    remote_file "/tmp/webtatic.rpm" do
-        source "https://mirror.webtatic.com/yum/el6/latest.rpm"
-        notifies :install, "rpm_package[webtatic-repo]", :immediately
-    end
+	webtatic_path = "#{node[:attachment_dir]}/webtatic.rpm"
 
-    rpm_package "webtatic-repo" do
-        action :nothing
-        source "/tmp/webtatic.rpm"
-    end
+	remote_file webtatic_path do
+		source "https://mirror.webtatic.com/yum/el6/latest.rpm"
+		notifies :install, "rpm_package[webtatic-repo]", :immediately
+		not_if { ::File.exists?("/etc/yum.repos.d/webtatic.repo") }
+	end
+
+	rpm_package "webtatic-repo" do
+		action :nothing
+		source webtatic_path
+	end
 
 	package "php56w"
-
-	directory "/etc/php5/conf.d" do
-		recursive true
-	end
 
 	%w(	php56w-gd
 		php56w-curl
 		php56w-mysql
 		php56w-xml
-		php56w-redis
+		php56w-pecl-redis
 		php-pear
 	  ).each do |p|
 		package p
