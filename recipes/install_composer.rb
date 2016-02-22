bc28fdad5542e3b50473c06135cf5485e9ff1d3b
Phil Dier <phil@raventools.com>
2016-02-22 16:09:05 -0600

    make data bag auth item work

--- recipes/install_composer.rb
+++ recipes/install_composer.rb
@@ -15,27 +15,34 @@ bash "update-composer" do
 	only_if { File.exists?("/usr/bin/composer") }
 end
 
-# load auth from data bag if available
-begin
-	composer_auth = Chef::EncryptedDataBagItem.load("composer", "auth")
-rescue
-	composer_auth = {}
-end
+ruby_block "fetch composer auth" do
+	block do
 
-puts composer_auth
+		begin
+			# use data bag item if available
+			composer_auth = Chef::EncryptedDataBagItem.load("composer", "auth").to_hash
+		rescue
+			# otherwise, fall back to chef attributes
+			composer_auth = {
+				"username" => node[:raven_php][:satis][:username],
+				"password" => node[:raven_php][:satis][:password]
+			}
+		end
+
+		# modifies the file resource below during runtime to include the correct auth info
+		resources("file[/var/lib/composer/auth.json]").content = JSON.generate({
+			"http-basic" => {
+				"#{node[:raven_php][:satis][:hostname]}" => composer_auth
+			}
+		})
+
+	end
+end
 
 # set up auth credentials for our local satis mirror
 directory "/var/lib/composer" do
     owner node[:raven_php][:composer][:owner]
     group node[:raven_php][:composer][:group]
 end
-file "/var/lib/composer/auth.json" do
-    content JSON.generate({
-                "http-basic" => {
-                    "#{node[:raven_php][:satis][:hostname]}" => {
-                        "username" => composer_auth["username"] || node[:raven_php][:satis][:username],
-                        "password" => composer_auth["password"] || node[:raven_php][:satis][:password]
-                    }
-                }
-            })
-end
+
+file "/var/lib/composer/auth.json"
