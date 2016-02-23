84b7160214d66b3ea2cd2a9e5830efa22a1b1d4f
Phil Dier <phil@raventools.com>
2016-02-23 09:52:15 -0600

    namespaces. how do they work?

--- recipes/install_composer.rb
+++ recipes/install_composer.rb
@@ -22,11 +22,11 @@ ruby_block "fetch composer auth" do
 
 			# use data bag item if available
 			composer_auth = Chef::EncryptedDataBagItem.load("composer", "auth").to_hash
-			log "Successfully read data bag\n";
+			::Chef::log "Successfully read data bag\n";
 
 		rescue Exception => ex
 
-			log "Failed reading data bag: #{ex.class} #{ex.message}"
+			::Chef::log "Failed reading data bag: #{ex.class} #{ex.message}"
 
 			# otherwise, fall back to chef attributes
 			composer_auth = {
@@ -35,7 +35,7 @@ ruby_block "fetch composer auth" do
 			}
 		end
 		
-		log composer_auth
+		::Chef::log "auth: #{composer_auth}"
 
 		# modifies the file resource below during runtime to include the correct auth info
 		resources("file[/var/lib/composer/auth.json]").content JSON.generate({
