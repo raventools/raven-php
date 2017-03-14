e6d003c9e4872d62739cf4f2f7aaacbbd95e56be
Byron Albert <byron@raventools.com>
2017-03-14 14:56:29 -0500

    update configure recipe and setup var's

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,3 +17,6 @@ default[:raven_php][:xdebug][:remote_port] = 9000
 default[:raven_php][:xdebug][:remote_handler] = "dbgp"
 default[:raven_php][:xdebug][:remote_log] = "/var/log/xdebug.log"
 default[:raven_php][:xdebug][:remote_host] = "10.0.2.2"
+
+
+default[:raven_php][:php_conf] = ["date.timezone = US/Eastern","memory_limit = 128M","allow_url_fopen=1","expose_php = off"]
--- recipes/configure_php.rb
+++ recipes/configure_php.rb
@@ -1,2 +1,18 @@
 
 #
+# generates php conf overrides
+
+service "httpd" do
+	supports :reload => true
+	action :nothing
+end
+
+template "/etc/php.d/custom.ini" do
+	source "php.ini.erb"
+	user "root"
+	mode 0644
+	variables ({
+		:parameters => node[:raven_php][:php_conf]
+	})
+	notifies :reload, "service[httpd]", :delayed
+end
